376f754faa0a039b72b7d6201471d8dbefed755b
ginokent <29125616+ginokent@users.noreply.github.com>
2023-11-09 06:09:42 +0900

    fix: fix comment regex

--- internal/ddlgen/lang/go/extract_source.go
+++ internal/ddlgen/lang/go/extract_source.go
@@ -34,9 +34,9 @@ var (
 const (
 	//	                                          ________________ <- 1. comment prefix
 	//	                                                          __ <- 2. tag name
-	//	                                                                          ___ <- 4. comment suffix
-	_DDLTagGoCommentLineRegexFormat       = `^\s*(//+\s*|/\*\s*)?(%s):\s*(.*)?\s*(\*/)?`
-	_DDLTagGoCommentLineRegexContentIndex = /*                            ^^ 3. tag value */ 3
+	//	                                                                             ___ <- 4. comment suffix
+	_DDLTagGoCommentLineRegexFormat       = `^\s*(//+\s*|/\*\s*)?(%s)\s*:\s*(.*)?\s*(\*/)?`
+	_DDLTagGoCommentLineRegexContentIndex = /*                               ^^ 3. tag value */ 3
 )
 
 func DDLTagGoCommentLineRegex() *regexp.Regexp {
